45e03fb0f7c2d0bbd3cc30581b30d52c000a8a4b
Ivan Kutyrev <ivan.kutyrev.06@mail.ru>
2026-01-12 23:45:02 +0500

    Скрыл пагинацию для поломанного теста

--- yatube_api/api/pagination.py
+++ yatube_api/api/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 
 class PostLimitOffsetPagination(LimitOffsetPagination):
@@ -6,3 +7,6 @@ class PostLimitOffsetPagination(LimitOffsetPagination):
     limit_query_param = 'limit'
     offset_query_param = 'offset'
     max_limit = 100
+
+    def get_paginated_response(self, data):
+        return Response(data)
